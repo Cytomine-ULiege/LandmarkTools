6404c53d91e530c66ece38ce0454f33af4b8f09a
Rémy Vandaele <remy.vandaele@gmail.com>
2019-02-19 10:50:48 +0100

    changes

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='LandmarkTools',
-    version='0.0.2a',
+    version='0.0.2b',
     description='Tools for the Landmark Detection algorithms.',
     long_description=long_description,
     long_description_content_type="text/markdown",
